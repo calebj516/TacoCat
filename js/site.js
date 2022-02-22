92d9fd1f021829a69d845cb3b32da904c42fa5d8
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-21 21:15:43 -0600

    Taco Cat: Initial Commit and App Page
    Pulled down Js Site Template, set up app.html and built out the logic behind the application in site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
-// call Hello World
-function HelloWorld() {
-  alert("Hello World!");
+// Get Values
+function getValues() {
+  // Get string from user
+  let str = document.getElementById('userInput').value;
+  // Pass string to isPalindrome
+  let status = isPalindrome(str);
+  // Pass result of isPalindrome to displayResults
+  displayResults(status, str);
+}
+
+// Determine Palindrome Status
+function isPalindrome(str) {
+  // create a reversed copy of str
+  let reversedStr = str.split('').reverse().join('');
+  // return result
+  return str === reversedStr;
+}
+
+// Display Results
+function displayResults(status, str) {
+  let reversedStr = str.split('').reverse().join('');
+  // write to the page
+  if(status){
+    alert(`Success! ${reversedStr} is a palindrome!`);
+    // document.getElementById('msg').innerHTML = `Success! ${reversedStr} is a palindrome!`;
+    // document.getElementById('alert').classList.remove('invisible');
+  } else {
+    alert(`Too bad! ${str} is not a palindrome.`);
+    // document.getElementById('msg').innerHTML = `Too bad! ${reversedStr} is not a palindrome.`;
+    // document.getElementById('alert').classList.remove('invisible');
+  }
 }
