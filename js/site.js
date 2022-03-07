2e56810ecb105486ba7a2b8614f8ef9b1e3a2ed6
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-03-06 20:59:58 -0600

    Taco Cat: App Page and Site JS
    Updated app.html and site.js to show an alert displaying whether the user-entered string is a palindrome along with the reversed string. Modifications made based on Coder Foundry walkthrough video.

--- js/site.js
+++ js/site.js
@@ -1,32 +1,43 @@
 // Get Values
 function getValues() {
+  // make sure the alert is invisble
+  document.getElementById('alert').classList.add('invisible');
   // Get string from user
   let str = document.getElementById('userInput').value;
   // Pass string to isPalindrome
-  let status = isPalindrome(str);
+  let returnObj = isPalindrome(str);
   // Pass result of isPalindrome to displayResults
-  displayResults(status, str);
+  displayResults(returnObj);
 }
 
 // Determine Palindrome Status
 function isPalindrome(str) {
-  // create a reversed copy of str
-  let reversedStr = str.split('').reverse().join('');
-  // return result
-  return str === reversedStr;
+  // lowercase and remove special characters
+  str = str.toLowerCase();
+  // remove special characters
+  let regex = /[^a-z0-9]/gi;
+  str = str.replace(regex, '');
+  // create a reversed copy of str and an object to hold the result message and reversed string
+  let reversedStr = '';
+  let returnObj = {};
+  // loop through str in reverse, adding each element to reversedStr
+  for(let i = str.length - 1; i >= 0; i--){
+    reversedStr += str[i];
+  }
+  // return result message
+  str === reversedStr ? returnObj.msg = "Well done! You entered a Palindrome!" : returnObj.msg = "Sorry! You did not enter a Palindrome.";
+  // add a property to returnObj called reversed which holds the reversed string
+  returnObj.reversed = reversedStr;
+  // return the object holding result information
+  return returnObj;
 }
 
 // Display Results
-function displayResults(status, str) {
-  let reversedStr = str.split('').reverse().join('');
-  // write to the page
-  if(status){
-    alert(`Success! ${reversedStr} is a palindrome!`);
-    // document.getElementById('msg').innerHTML = `Success! ${reversedStr} is a palindrome!`;
-    // document.getElementById('alert').classList.remove('invisible');
-  } else {
-    alert(`Too bad! ${str} is not a palindrome.`);
-    // document.getElementById('msg').innerHTML = `Too bad! ${reversedStr} is not a palindrome.`;
-    // document.getElementById('alert').classList.remove('invisible');
-  }
+function displayResults(returnObj) {
+  // display return message in alert header
+  document.getElementById('alertHeader').innerHTML = returnObj.msg;
+  // show the reversed user input in the message
+  document.getElementById('msg').innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+  // unhide the alert
+  document.getElementById('alert').classList.remove('invisible');
 }
